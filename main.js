6e157f2a810e6e6b7611cd4670585fa163353d8f
Doria May <112048607+DoriaMay@users.noreply.github.com>
2024-06-16 15:44:53 +0530

    project images

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// header
 const menuBtn = document.querySelector(".menu-btn");
         const navigation = document.querySelector(".navigation");
 
@@ -7,3 +8,22 @@ const menuBtn = document.querySelector(".menu-btn");
             navigation.classList.toggle("active");
         });
     
+//Project menu
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += ent.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+});
+backBtn.addEventListener("click", () =>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
+});
